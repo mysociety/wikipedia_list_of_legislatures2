04ca5f081e71481632232b59de4c8321041110ab
Duncan Parkes <duncan@mysociety.org>
2015-02-18 13:46:12 +0000

    Fix encoding problem.
    lxml was getting into an encoding mess and giving back some unicode
    strings and some byte strings. Switch to BeautifulSoup4 with the
    html.parser backend in order to get consistent unicode.

--- scraper.py
+++ scraper.py
@@ -1,23 +1,24 @@
 # This is a Morph (https://morph.io) scaper for the list of legislatures from wikipedia
 # including some code snippets below that you should find helpful
 
+import hashlib
 import scraperwiki
-import lxml.html
+import requests
+from bs4 import BeautifulSoup
 
 source_url = 'http://en.wikipedia.org/wiki/List_of_legislatures_by_country'
-html = scraperwiki.scrape(source_url)
-root = lxml.html.fromstring(html)
+html = requests.get(source_url).text
+soup = BeautifulSoup(html, 'html.parser')
 
-title_span = root.cssselect('span#Legislatures_of_UN_member_states')[0]
-data_table = title_span.getparent().getnext()
-assert data_table.tag == 'table'
+title_span = soup.find('span', {'id': 'Legislatures_of_UN_member_states'})
+data_table = title_span.parent.find_next('table')
 
 
 class WikiTable(object):
   def __init__(self, table_element):
     self.element = table_element
     self.column_indices = dict(enumerate(
-      [(x.text or x.find('a').text).strip() for x in self.element.find('tr').findall('th')]
+      [(x.text or x.find('a').text).strip() for x in self.element.find('tr').find_all('th')]
       ))
     print self.column_indices
 
@@ -28,8 +29,8 @@ class WikiTable(object):
     remaining_rowspans = [0] * len(self.column_indices)
     data = {}
 
-    for row in self.element.findall('tr')[1:]:
-      tds = row.findall('td')
+    for row in self.element.find_all('tr')[1:]:
+      tds = row.find_all('td')
       rowspan_count = 0
       for col, rowspan in enumerate(remaining_rowspans):
         if rowspan:
@@ -39,21 +40,23 @@ class WikiTable(object):
 
         td = tds[col - rowspan_count]
 
-        found_rowspan = td.attrib.get('rowspan')
+        found_rowspan = td.get('rowspan')
         if found_rowspan:
           remaining_rowspans[col] = int(found_rowspan) - 1
 
         key = self.column_indices.get(col)
         data[key] = self.get_data(key, td)
 
-      data['id'] = '-'.join([data[id_key] for id_key in id_keys])
+      data['id'] = hashlib.md5(
+        (u'-'.join([data[id_key] for id_key in id_keys])).encode('utf_8')
+        ).hexdigest()
 
       scraperwiki.sqlite.save(unique_keys=('id',), data=data)
 
 
 class UNMembersTable(WikiTable):
   def get_data(self, key, td):
-    return td.text_content()
+    return ' '.join(td.stripped_strings)
 
 
 UNMembersTable(data_table).store_data(
